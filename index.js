598f11547373891ddc224d388b561bd0ae3baa47
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 20:52:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function getRepositories() {
   req.open("GET", `https://api.github.com/users/${ username }/repos`)
   req.send()
 }
+
+function showRepositories(event, data) {
+  let repos = JSON.parse(this.responseText)
+  console.log(repos)
+  const repoList = `<ul>${repos.map(r => '<li>' + r.name + ' - <a href="#" data-repo="' + r.name + '" onclick="getCommits(this)">Get Commits</a></li>').join('')}</ul>`
+  document.getElementById("repositories").innerHTML = repoList
+}
