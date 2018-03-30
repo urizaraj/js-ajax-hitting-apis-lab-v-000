559d1c247fa9c81fc457d0b4bad9e9a9710120ea
Alexander Urizar <urizaraj@gmail.com>
2018-03-30 20:50:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const usernameField = document.getElementByID('username')
 
 function getRepositories() {
-  let username =
+  let username = usernameField.value
   const req = new XMLHttpRequest()
   req.addEventListener("load", showRepositories);
   req.open("GET", `https://api.github.com/users/${ username }/repos`)
